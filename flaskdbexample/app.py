fc439e589ed8f65fe6ab8db082098e2f477f3945
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2020-06-21 13:35:50 -0400

    db api

---
+++ flaskdbexample/app.py
@@ -0,0 +1,85 @@
+
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/EmployeeDB'
+db = SQLAlchemy(app)
+
+###Models####
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    productDescription = db.Column(db.String(100))
+    productBrand = db.Column(db.String(20))
+    price = db.Column(db.Integer)
+
+    def create(self):
+      db.session.add(self)
+      db.session.commit()
+      return self
+    def __init__(self,title,productDescription,productBrand,price):
+        self.title = title
+        self.productDescription = productDescription
+        self.productBrand = productBrand
+        self.price = price
+    def __repr__(self):
+        return '' % self.id
+db.create_all()
+class ProductSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Product
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    title = fields.String(required=True)
+    productDescription = fields.String(required=True)
+    productBrand = fields.String(required=True)
+    price = fields.Number(required=True)
+
+@app.route('/products', methods = ['GET'])
+def index():
+    get_products = Product.query.all()
+    product_schema = ProductSchema(many=True)
+    products = product_schema.dump(get_products)
+    return make_response(jsonify({"product": products}))
+@app.route('/products/<id>', methods = ['GET'])
+def get_product_by_id(id):
+    get_product = Product.query.get(id)
+    product_schema = ProductSchema()
+    product = product_schema.dump(get_product)
+    return make_response(jsonify({"product": product}))
+@app.route('/products/<id>', methods = ['PUT'])
+def update_product_by_id(id):
+    data = request.get_json()
+    get_product = Product.query.get(id)
+    if data.get('title'):
+        get_product.title = data['title']
+    if data.get('productDescription'):
+        get_product.productDescription = data['productDescription']
+    if data.get('productBrand'):
+        get_product.productBrand = data['productBrand']
+    if data.get('price'):
+        get_product.price= data['price']    
+    db.session.add(get_product)
+    db.session.commit()
+    product_schema = ProductSchema(only=['id', 'title', 'productDescription','productBrand','price'])
+    product = product_schema.dump(get_product)
+    return make_response(jsonify({"product": product}))
+@app.route('/products/<id>', methods = ['DELETE'])
+def delete_product_by_id(id):
+    get_product = Product.query.get(id)
+    db.session.delete(get_product)
+    db.session.commit()
+    return make_response("",204)
+@app.route('/products', methods = ['POST'])
+def create_product():
+    data = request.get_json()
+    product_schema = ProductSchema()
+    product = product_schema.load(data)
+    result = product_schema.dump(product.create())
+    return make_response(jsonify({"product": result}),200)
+if __name__ == "__main__":
+    app.run(debug=True)
+
